52ac2297f79ef129ecde306cc4aecbce6e708c1a
hua5155 <s17115155@stu.edu.tw>
2024-07-15 11:44:16 +0800

    Updated chat icon.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,7 +27,7 @@
 	class="relative h-screen w-screen overflow-hidden bg-[#008080] font-['Source_Code_Pro','Noto_Sans_TC'] text-black"
 >
 	<div class="grid h-fit w-fit grid-flow-row *:select-none">
-		<Icon url="/icon/chat.svg" id={chatSetting.id} iconName="chat"></Icon>
+		<Icon url="/icon/chat.svg" id={chatSetting.id} iconName="Chat"></Icon>
 		<Icon url="/icon/cli.svg" id={cliSetting.id} iconName="CLI"></Icon>
 	</div>
 	<Chat bind:chat={data.messages}></Chat>
